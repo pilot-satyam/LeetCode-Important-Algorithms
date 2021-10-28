e141d3f5a1a6f379cb2b38c47274167528a38ac4
Satyam Srivastava <62789503+pilot-satyam@users.noreply.github.com>
2021-10-28 23:15:54 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,3 +1,46 @@
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        if(nums.size() < 3) return res;
+        
+        //1. Sort the given vector
+        sort(nums.begin(), nums.end());
+        
+        //a+b+c = 0 => b+c = -a
+        //Moving for value of 'a' (a=num[i]) which is not a duplicate
+        for(int i=0; i<nums.size()-2; i++){
+            
+            if(i==0 || nums[i] != nums[i-1]){
+                int low = i+1, high = nums.size()-1, sum = 0-nums[i];
+                //sum is the pair we are finding in the array
+                
+                while(low < high){
+                    //If a pair is found, insert triplet in res
+                    if(nums[low] + nums[high] == sum){
+                        vector<int> temp;
+                        temp.push_back(nums[i]); //a
+                        temp.push_back(nums[low]); //b
+                        temp.push_back(nums[high]); //c
+                        res.push_back(temp);
+                        
+                        //Now move low forward and high backwards, until they skip the current element
+                        while(low<high && nums[low] == nums[low+1]) low++;
+                        while(low<high && nums[high] == nums[high-1]) high--;
+                        low++;
+                        high--;
+                    }
+                    else if(nums[low] + nums[high] < sum) low++;
+                    else if(nums[low] + nums[high] > sum) high--;
+                }            }
+        }
+        return res;
+    }
+};
+
+//2nd Method
+
 /*
 Two Pointer Approach:
 The basic thinking logic for this is:
@@ -48,10 +91,8 @@ public:
                         
                             high--;
                     }
-                       
                    
                     continue;
-                 
                 }
                 else if(b+c<x && low<n)
                    { low++;continue;}
@@ -61,9 +102,9 @@ public:
             
             if(low>=high || (nums[i]==a && i>0))
                 i++;
-            
         }
-       
         return ans;
     }
 };
+
+
