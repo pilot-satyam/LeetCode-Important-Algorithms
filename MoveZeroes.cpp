40e3d8899a190340bcd702c36c625c8efd2d78ce
Satyam Srivastava <62789503+pilot-satyam@users.noreply.github.com>
2021-11-13 12:56:37 +0530

    Create MoveZeroes.cpp

---
+++ MoveZeroes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+   /*void moveZeroes(vector<int>& nums) {
+    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+        if (nums[cur] != 0) {
+            swap(nums[lastNonZeroFoundAt++], nums[cur]);
+        }
+    }
+}
+}; */
+
+//or
+ 
+void moveZeroes(vector<int>& nums) {
+    int n = nums.size();
+
+    // Count the zeroes
+    int numZeroes = 0;
+    for (int i = 0; i < n; i++) {
+        numZeroes += (nums[i] == 0);
+    }
+
+    // Make all the non-zero elements retain their original order.
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != 0) {
+            ans.push_back(nums[i]);
+        }
+    }
+
+    // Move all zeroes to the end
+    while (numZeroes--) {
+        ans.push_back(0);
+    }
+
+    // Combine the result
+    for (int i = 0; i < n; i++) {
+        nums[i] = ans[i];
+    }
+}
+};
